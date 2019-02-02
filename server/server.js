53cb822e20dc598475cbf3eed701073a4eb96d6e
Senderhauf <centerhauf@gmail.com>
2019-02-02 16:09:37 -0600

    delete endpoint for api working

--- server/server.js
+++ server/server.js
@@ -83,44 +83,66 @@ app.post('/api/courses/addpreq/type/:courseType/value/:courseValue', (req, res) 
     let newCourse = req.body;
 
     db.collection('comp_sci_courses').find({courseType:req.params.courseType, courseValue: req.params.courseValue}).toArray().then(course => {
-        console.log(`newCourse: ${JSON.stringify(newCourse)}`)
-        console.log(`course[0].prereqs before: ${JSON.stringify(course[0])}`)
         course[0].prereqs.push(newCourse)
-        console.log(`course[0].prereqs after: ${course[0].prereqs}`)
         updatedCourse = course[0]
-        console.log('UpdatedCourse: ' + JSON.stringify(updatedCourse))
+        return updatedCourse
+    }).then(updatedCourse => {
+        if(updatedCourse == null){
+            throw Error("updatedCourse is null")
+        }
+
+        db.collection('comp_sci_courses').updateOne({courseType:req.params.courseType, courseValue: req.params.courseValue}, {$set: updatedCourse}, {upsert:true}).then(updatedCourse => {
+            console.log(`server newCourse: ${updatedCourse}`)
+            res.json(updatedCourse);
+        }).catch(error => {
+            console.log(error);
+            res.status(500).json({message: `Internal Server Error: ${error}`});
+        });
+
     }).catch(error => {
         console.log('Error: '+error);
         res.status(500).json({ message: `Internal Server Error: ${error}`});
     });
 
-    if(updatedCourse == null) {
-        res.status(422).json({message: `Invalid request: updatedCourse == ${updatedCourse}`});
-        return;
-    }
+});
 
-    db.collection('comp_sci_courses').updateOne({courseType:req.params.courseType, courseValue: req.params.courseValue}, {$set: updatedCourse}, {upsert:true}).then(result =>
-        {
-            console.log(`modified count: ${result.modifiedCount}`)
-            console.log(`matched count: ${result.matchedCount}`)
+app.delete('/api/courses/type/:courseType/value/:courseValue/', (req, res) => {
+    
+    let paramCourseType = req.params.courseType
+    let paramCourseValue = req.params.courseValue
 
-            if (!(result.matchedCount === 0 && result.modifiedCount === 0)){
-                throw Error('Course Exists Already')
-            }
-            console.log(`result.upsertedId: ${result.upsertedId._id}`)
-            var o_id = new mongo.ObjectID(result.upsertedId._id)
-            console.log(`objectid: ${o_id}`)
-            return db.collection('comp_sci_courses').find({_id: o_id }).limit(1).next()
-        }
-	).then(updatedCourse => {
-        console.log(`server newCourse: ${updatedCourse}`)
-		res.json(updatedCourse);
-	}).catch(error => {
-		console.log(error);
-		res.status(500).json({message: `Internal Server Error: ${error}`});
-	});
-})
+    // First Query: delete the course from the comp_sci_courses collection 
+    db.collection('comp_sci_courses').deleteOne({courseType:paramCourseType, courseValue:paramCourseValue}).then(deletedCourse => {
+        console.log(`deleted course: ${JSON.stringify(deletedCourse)}`)
+    }).then(
 
-// app.delete('/api/courses/type/:courseType/value/:courseValue/prereq/:prereqID'), (req, res) => {
-//     //TODO
-// }
+        // Second Query: delete the course from any remaining course's prereqs field
+        // Note: if the prereq is an OR prereq and the deleted course is contained within, the entire OR prereq will be deleted
+        db.collection('comp_sci_courses').find().toArray().then(courses => {
+            courses.map((course) => {
+                    console.log(`\n\nCOURSE: ${JSON.stringify(course)}`)
+                    let prereqs = course.prereqs
+                    console.log(`\nCOURSE PREREQS: ${JSON.stringify(prereqs)}`)
+
+                    let filteredPrereqs = prereqs.filter(item => 
+                        (item.or == null && !(item.courseType === paramCourseType && item.courseValue === paramCourseValue))  
+                        ||  
+                        (item.or != null && item.or.filter(x => x.courseType === paramCourseValue && x.courseValue === paramCourseValue).length === 0)
+                    )
+
+                    console.log(`\nFILTERED PREREQS: ${JSON.stringify(filteredPrereqs)}`)
+                    
+                    course.prereqs = filteredPrereqs
+
+                    db.collection('comp_sci_courses').updateOne({courseType:course.courseType, courseValue: course.courseValue}, {$set: course}, {upsert:true})
+                    .catch(error => {
+                        console.log(error);
+                        res.status(500).json({message: `Internal Server Error: ${error}`});
+                    });
+            })
+            res.status(200).json({message: `Course Removed: ${req.params.courseType} ${req.params.courseValue}`})
+    })).catch(error => {
+        console.log(error);
+        res.status(500).json({ message: `Internal Server Error: ${error}`});
+    });
+});
